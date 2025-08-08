1c0deb58ca84320df0efa80f10f6a5408c613d05
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-08 13:26:58 +0530

    add more scenarios

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,7 +13,38 @@ TEST(Statistics, ReportsAverageMinMax) {
     EXPECT_LT(fabsf(computedStats.min - 1.5), epsilon);
 }
 
-//TEST(Statistics, AverageNaNForEmpty) {
-//    float values[1] = {};
-//    auto computedStats = compute_statistics(values, 0);
-//}
+TEST(Statistics, ReportAverageNaNForEmpty) {
+    float values[1] = {};
+    auto computedStats = compute_statistics(values, 0);
+    EXPECT_TRUE(isnan(computedStats.average));
+    EXPECT_TRUE(isnan(computedStats.max));
+    EXPECT_TRUE(isnan(computedStats.min));
+}
+
+TEST(Statistics, ReportAverageNaNForAllNaNValues) {
+    float values[] = {NAN, NAN, NAN};
+    auto computedStats = compute_statistics(values, 3);
+    EXPECT_TRUE(isnan(computedStats.average));
+    EXPECT_TRUE(isnan(computedStats.max));
+    EXPECT_TRUE(isnan(computedStats.min));
+}
+
+TEST(Statistics, ReportAverageIgnoreSingleNaN) {
+    float values[] = {1.0, 2.0, NAN, 4.0};
+    auto computedStats = compute_statistics(values, 4);
+    float epsilon = 0.001;
+    
+    EXPECT_LT(fabsf(computedStats.average - 2.333), epsilon);
+    EXPECT_LT(fabsf(computedStats.max - 4.0), epsilon);
+    EXPECT_LT(fabsf(computedStats.min - 1.0), epsilon);
+}
+
+TEST(Statistics, ReportAverageNaNForAbsurdValues) {
+    float values[] = {1e5, 1.0, -1, 3.0, 110000};
+    auto computedStats = compute_statistics(values, 5);
+    float epsilon = 0.001;
+
+    EXPECT_TRUE(isnan(computedStats.average));
+    EXPECT_TRUE(isnan(computedStats.max));
+    EXPECT_TRUE(isnan(computedStats.min));
+}
--- stats.c
+++ stats.c
@@ -1,12 +1,22 @@
 #include "stats.h"
 #include <stddef.h>
+#include <math.h>
 
 struct Stats compute_statistics(const float* numbers, int count) {
     float sum = 0.0f;
     float min = 0.0f;
     float max = 0.0f;
+    int validCount = 0;
+    int invalid = 0;
 
     for (int i = 0 ; i < count; i++) {
+        if (isnan(numbers[i])) {
+            continue; // Ignore NaN values
+        }
+        invalid = numbers[i] < 0 || numbers[i] > 200;
+        if (invalid) {
+            break; // Break on absurd values
+        }
         if (i == 0) {
             min = numbers[i];
             max = numbers[i];
@@ -19,10 +29,18 @@ struct Stats compute_statistics(const float* numbers, int count) {
             }
         }
         sum += numbers[i];
+        ++validCount;
     }
+    printf("Sum: %f, Min: %f, Max: %f\n", sum, min, max);
     struct Stats stats;
-    stats.average = sum / count;
-    stats.min = min;
-    stats.max = max;
+    if (count == 0 || validCount == 0 || invalid) {
+        stats.average = NAN;
+        stats.min = NAN;
+        stats.max = NAN;
+    } else {
+        stats.average = sum / validCount;
+        stats.min = min;
+        stats.max = max;
+    }
     return stats;
 }
